259fdf903fcf47c949650613d32616ada4e59b4a
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-06-27 21:26:10 -0300

    Update jsEntradaSalida-9.js

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,8 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+var sueldo;
+sueldo=document.getElementById("importe").value;
+var resultado=(sueldo/10);
 
-	
 }
