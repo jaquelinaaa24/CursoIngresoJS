a71f19a16da1e0fca080b9b787084e96d0f57406
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-06-27 21:24:39 -0300

    Update jsEntradaSalida-6.js

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -2,7 +2,13 @@
 transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
+
 {
-
+var numero1;
+var numero2;
+var resultado;
+numero1=document.getElementById("numeroUno").value;
+numero2=document.getElementById("numeroDos").value;
+resultado=(parseInt(numero1) + parseInt(numero2));
+alert("El resultado de la suma de los numeros que usted ingreso es: " +resultado);
 }
-
