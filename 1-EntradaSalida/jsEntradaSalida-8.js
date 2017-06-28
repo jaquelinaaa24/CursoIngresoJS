aa57b7bf8a57f421f0f4c16bfcb6c16b21c562a5
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-06-27 21:25:20 -0300

    Update jsEntradaSalida-8.js

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -3,6 +3,13 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
+
 {
-	
-}
+var dividendo
+var dividendo=document.getElementById("dividendo");
+var divisor;
+var divisor=document.getElementById("divisor");
+var resto;
+resto=(parseInt (dividendo) % parseInt (divisor));
+alert("El resto es: " +resto); } //ME DA "NAN"
+
