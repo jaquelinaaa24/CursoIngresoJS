9fa840440ccbdb7d6150f408f34461fd4a4426b3
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-01 12:55:34 -0300

    Update jsEntradaSalida-9.js

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,7 +5,12 @@ en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
 var sueldo;
+var aumento;
+var resultado;
 sueldo=document.getElementById("importe").value;
-var resultado=(sueldo/10);
+var aumento=(sueldo*0,10);// esto es equivalente a hacer (sueldo*1,1)
+var resultado=(sueldo+aumento);
+
+
 
 }
