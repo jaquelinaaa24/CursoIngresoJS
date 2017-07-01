b707a1c2c43f67ce74af704dfaa9b847d8e1b559
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-01 12:59:12 -0300

    Update jsEntradaSalida-10.js

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,13 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+var importe;
+var descuento;
+var importeFinal;
+importe=document.getElementById("importe").value;
+var descuento=(importe*0.25);
+var importeFinal=(importe-descuento);
+
+
+
 }
