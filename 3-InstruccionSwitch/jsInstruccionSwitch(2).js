8242aa33fcec4ac84ef1d2ddbcb9d370571237fc
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-03 12:18:25 -0300

    Update jsInstruccionSwitch(2).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -36,3 +36,32 @@ default:
  alert("Ya pasamos el frio, ahora calor!!!." ); 
 }
 }
+
+/*al seleccionar un mes informar. 
+si estamos en Invierno: "Abrigate que hace frio." 
+si aún no llego el Invierno: "Falta para el invierno." 
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!." 
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno. */
+
+
+function Mostrar()
+{
+
+var mes = document.getElementById('mes').value;
+
+if(mes=="Julio" || mes=="Agosto")
+{
+ alert("Abrigate que hace frio");
+}
+
+else if(mes=="Enero" || mes=="Febrero" || mes=="Marzo" || mes=="Abril" || mes=="Mayo" || mes=="Junio")
+{
+ alert("Falta para el invierno");
+}
+
+else 
+{
+  alert("Ya pasamos el frio, ahora calor!!!");  
+}
+ 
+*/
