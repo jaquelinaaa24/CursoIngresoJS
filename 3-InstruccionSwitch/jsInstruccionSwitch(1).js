0c0b61dc1509cc73dcd9dda4e16c764a3100e81f
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-03 12:10:16 -0300

    Update jsInstruccionSwitch(1).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -38,3 +38,41 @@ break;
  
 }
 }
+
+
+
+
+
+/* HECHA CON IF  al seleccionar un mes informar. 
+si es Enero: "que comiences bien el año!!!." 
+si es Marzo: "a clases!!!." 
+si es Julio: "se vienen las vacaciones!!!." 
+si es Diciembre: "Felices fiesta!!!." */
+
+function Mostrar()
+{
+  
+var mes= document.getElementById('mes').value;
+
+if(mes=="Enero")
+{
+ alert("que comiences bien el año!!!.");
+}
+else if(mes=="Marzo")
+{
+ alert("a clases!!!!!");
+}
+else if(mes=="Julio")
+{
+alert("Se vienen vacaciones");
+}
+else if (mes=="Diciembre")
+{
+ alert("Felices Fiestas!!") ;  
+}
+else 
+{
+    alert(" ");
+}
+}
+*/
