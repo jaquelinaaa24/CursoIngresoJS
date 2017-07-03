f0ffc90ec5f4d658830b54257f9932ac4ca210f1
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-02 23:13:13 -0300

    Update jsEntradaSalida-9.js

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,12 +4,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
-var sueldo;
-var aumento;
-var resultado;
-sueldo=document.getElementById("importe").value;
-var aumento=(sueldo*0,10);// esto es equivalente a hacer (sueldo*1,1)
-var resultado=(sueldo+aumento);
+  var sueldo; 
+  var aumento;
+  var resultado;
+  sueldo=parseInt(document.getElementById("sueldo").value);
+  aumento=sueldo*0.10 ;// esto es equivalente a hacer (sueldo*1,1)
+  resultado=(sueldo+aumento);
+  document.getElementById("resultado").value=resultado; 
 
 
 
