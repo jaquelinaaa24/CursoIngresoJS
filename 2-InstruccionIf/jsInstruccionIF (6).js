2298e9cc96dda47a381ecae49f027e58255f78b7
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-02 23:18:04 -0300

    Update jsInstruccionIF (6).js

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,8 +1,28 @@
+/*Al ingresar una edad debemos informar si
+ la persona es mayor de edad (mas de 18 años) o 
+adolescente (entre 13 y 17 años) o niño 
+(menor a 13 años).*/
+
+
 function Mostrar()
 {
-//tomo la edad  
-
-
-
-
-}//FIN DE LA FUNCIÓN
+  var edad;
+  edad=document.getElementById("edad").value;
+  
+  if(edad>=18)
+  {
+  alert("La persona es mayor de edad");
+  }
+  else
+  {
+  if(edad>=13)
+  {
+  alert("La persona es adolescente");
+    }
+    else
+    {
+        alert("La persona es niño");
+    }
+  }
+ 
+}
