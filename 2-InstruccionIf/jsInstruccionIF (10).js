12d10f1dcb9371790ef175d6eb760d7e2eae9699
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-03 09:51:12 -0300

    Update jsInstruccionIF (10).js

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,31 @@
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+	/*Al presionar el Botón, asignar una nota
+ RANDOM al examen y mostar: "EXCELENTE"
+  para notas igual a 9 o 10, 
+"APROBÓ" para notas mayores a 4, "Vamos,
+ la proxima se puede" para notas menores a 4*/
 
-}//FIN DE LA FUNCIÓN
+function Mostrar()
+{
+var nota;
+nota=parseInt((Math.random()*(10-1))+1);
+
+
+if(nota>=9)	
+{
+	alert("EXCELENTE: " +nota);
+}
+else 
+{
+ if(nota>=4)
+ {
+ alert("APROBO: " +nota);
+ }
+ else
+  {
+  alert("VAMOS LA PROXIMA SE PUEDE: " +nota);
+  }
+
+}
+}
