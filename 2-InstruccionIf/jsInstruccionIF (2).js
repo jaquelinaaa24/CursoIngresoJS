273be024d26955b7eb3e1adb5951c25f7a058bee
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-02 23:15:33 -0300

    Update jsInstruccionIF (2).js

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,14 @@
+/*Al ingresar una edad debemos informar 
+si la persona es mayor de edad*/ 
+
 function Mostrar()
 {
-//tomo la edad  
+   var edad;
+   edad=parseInt(document.getElementById("edad").value);
 
+   if(edad>=18)
+   {
+   alert("La persona es mayor de edad");
+   }
 
-
-}//FIN DE LA FUNCIÓN
+}
