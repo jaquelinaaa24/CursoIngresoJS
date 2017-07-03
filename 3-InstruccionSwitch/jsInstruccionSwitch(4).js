bc5242a617cbcaffb6673904373bf5488d02f736
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-03 11:23:07 -0300

    Update jsInstruccionSwitch(4).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,41 @@
+/*al seleccionar un mes informar. 
+si tiene 28 días. 
+si tiene 30 días. 
+si tiene 31 días.*/
+
+
+
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+var mes = document.getElementById('mes').value;
 
+switch(mes)
+{
 
+ case"Enero":
+ case"Marzo":
+ case"Mayo":
+ case"Julio":
+ case"Agosto":
+ case"Octubre":
+ case"Diciembre":
+{
+  alert("Este mes tiene 31 dias");
+}
+ break;
 
-}//FIN DE LA FUNCIÓN
+ case"Noviembre":
+ case"Abril":
+ case"Junio":
+ case"Septiembre":
+ {
+  alert("Este mes tiene 30 dias");
+}
+ break;
+ default:
+ {
+ alert("Este mes tiene 28 dias");
+ }
+}
+}
