38ac39a2e607db48e524f0c6655340fe16d008c0
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-02 23:18:33 -0300

    Update jsInstruccionIF (7).js

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,21 @@
+/*Al ingresar una edad menor a 18 años y 
+un estado civil distinto a "Soltero",
+ mostrar el siguiente mensaje: 
+ 'Es muy pequeño para NO ser soltero.'*/
+
 function Mostrar()
 {
-//tomo la edad  
+ var edad;
+ var estadoCivil;
+ 
+ edad=document.getElementById("edad").value;
+ estadoCivil=document.getElementById("estadoCivil").value;
 
-	
+  if(edad<18 || estadoCivil!=soltero); //no tendria que ser un &&
+
+   {
+   alert("es muy pequeño para NO ser soltero");
+   }
 
 
-}//FIN DE LA FUNCIÓN
+ }
