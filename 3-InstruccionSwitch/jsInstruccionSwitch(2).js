4e0a16864529199404a0fd1718f4a82777fcd2b5
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-03 10:26:02 -0300

    Update jsInstruccionSwitch(2).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,38 @@
+/*al seleccionar un mes informar. 
+si estamos en Invierno: "Abrigate que hace frio." 
+si aún no llego el Invierno: "Falta para el invierno." 
+si ya paso el Invierno: "Ya pasamos el frio, ahora calor!!!." 
+ACLARAcIÓN: tomamos a Julio y Agosto como los meses de Invierno.*/
+
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+var mes = document.getElementById('mes').value;
 
+switch(mes)
+{
+case "Julio": 
+case "Agosto":
+{
+ alert("Abrigate que hace frio");
 
+}
+break
 
+case"Enero":
+case"Febrero":
+case"Marzo":
+case"Abril":
+case"Mayo":
+case"Junio":
 
-}//FIN DE LA FUNCIÓN
+{
+ alert("Falta para el invierno");
+}
+break;
+
+default:
+
+ alert("Ya pasamos el frio, ahora calor!!!." ); 
+}
+}
