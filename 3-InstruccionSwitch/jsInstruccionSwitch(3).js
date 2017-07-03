8b3c8d70d15c04039d4cf39c7325056f4005fd12
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-03 11:06:08 -0300

    Update jsInstruccionSwitch(3).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,23 @@
+/*al seleccionar un mes informar. 
+si es Febrero: " Este mes no tiene más de 29 días." 
+si NO es Febrero: "Este mes tiene 30 o más días"*/
+
+
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mes = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+switch(mes)
+{
+case"Febrero":
+{
+alert("Este mes no tiene más de 29 días.");
+}
+break;
+default:
 
+alert("Este mes tiene 30 o más días");
 
-}//FIN DE LA FUNCIÓN
+break;
+}
+}
