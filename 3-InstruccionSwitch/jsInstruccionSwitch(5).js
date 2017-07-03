428587934391d5db97c451cec737e96871096329
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-03 11:28:53 -0300

    Update jsInstruccionSwitch(5).js

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,12 +1,26 @@
+/*Al ingresar una hora, informar: 
+si está entre las 7 y las 11 : "Es de mañana.".*/
+
+
 function Mostrar()
 {
-//tomo la edad  
+
 var laHora = document.getElementById('hora').value;
 
-//alert (laHora);
+switch(laHora)
+{
+case "7": //tiene que ser del mismo tipo que el (laHora)
+case "8":
+case "9":
+case "10":
+case "11":
+{
+    alert("Es de mañana");
+}
+ break;
+
+
+
+}
 	
-	
-
-
-
-}//FIN DE LA FUNCIÓN
+}
