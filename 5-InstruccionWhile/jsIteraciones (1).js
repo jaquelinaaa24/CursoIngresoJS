482e64b4723cb8ba434e0309b318922602a9e266
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-05 11:37:12 -0300

    Update jsIteraciones (1).js

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,17 @@
+/*al presionar el botón mostrar 10 repeticiones 
+con números ASCENDENTE, desde el 1 al 10*/
+
+
 function Mostrar()
 {
-	alert('iteración while');
+var numero=0
+
+while(numero<10)
+{
+	numero++;
+	alert(numero);
+}
 
 
-}//FIN DE LA FUNCIÓN
+
+}
