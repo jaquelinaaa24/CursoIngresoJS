172aa4d8fdb679f3ae93ea59c3a289489fa87eae
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-05 12:11:06 -0300

    Update jsIteraciones (3).js

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,18 @@
-function Mostrar()
+/*Enunciado:
+al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
+
+function Mostrar() /*para validar una clave utulizamos el while tambien podemos usar 
+mientras eso sea true para validar tomamos los valoes EERRONEOS se pregunta por la negativa si esta EERRONEOS
+no sale del while , le damos un valor cualquiera para que cumpla con la funcion de ser true que es distinto*/
 {
 
-var clave = prompt("ingrese el número clave.");
+
+var clave="a";
 
 
-}//FIN DE LA FUNCIÓN
+while(clave!="utn750")
+{
+ clave = prompt("ingrese el número clave.");
+}
+
+}
