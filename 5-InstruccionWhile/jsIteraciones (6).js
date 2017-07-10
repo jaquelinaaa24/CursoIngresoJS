3aaa5e830ab219408432cf248e55e642c259176e
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-10 03:22:16 -0300

    Update jsIteraciones (6).js

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,19 @@
+/*Al presionar el botón pedir 5 números e informar 
+la suma acumulada y el promedio. */
+
+
 function Mostrar()
 {
-
+    var numero;
 	var contador=0;
 	var acumulador=0;
-
-
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
-
-}//FIN DE LA FUNCIÓN
+    var i=0
+while(contador<5)
+{
+ numero=parseInt(prompt("Por favor ingrese 5 numeros"));
+acumulador= acumulador +numero;
+ i= i+1;
+}
+document.getElementById("suma").value=acumulador;
+document.getElementById("promedio").value=acumulador/5;
+}
