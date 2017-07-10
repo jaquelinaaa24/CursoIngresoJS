3197a7f05c9f120ce402cce4068a9b6518764adb
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-10 10:37:30 -0300

    Update jsIteraciones (7).js

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,12 +1,32 @@
+
+/*Al presionar el botón pedir números hasta que 
+el usuario quiera e informar la suma acumulada y el promedio.*/
+
+
+
 function Mostrar()
 {
-
+    var numero=0;
 	var contador=0;
-	var acumulador=0;
-	var respuesta='si';
+	var acumulador=0
+	var respuesta=true;
 
+  
+    
+    while(respuesta==true) 
+   {
+	
+	contador++;
+	numero=prompt("Ingrese numero CORRECTO");
+    numero=parseInt(numero);
+    acumulador=acumulador+contador;
+	respuesta=confirm();
+    
+	
+   }
+   
+      
+      alert(acumulador);
+      alert(acumulador/contador);
+   }
 
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/contador;
-
-}//FIN DE LA FUNCIÓN
