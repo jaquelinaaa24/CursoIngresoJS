f88eefeb9c11ed663c1ecd88889cba3639eae42d
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-10 12:23:31 -0300

    Update jsIteraciones (8).js

--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -1,14 +1,37 @@
+/*Al presionar el botón pedir números hasta que el
+ usuario quiera, sumar los que son positivos y multiplicar los negativos.*/
+
+
+
 function Mostrar()
 {
+   var numero=0;
+   var acumulador=0;
+   var contador=0; //el nuetro de la multiplicacion es 1 en la suma es 0
+   var respuesta=true;
+   var acumuladorSuma;
+   var acumuladorMultiplicacion;
 
-	var contador=0;
-	var positivo=0;
-	var negativo=1;
-	
-	var respuesta='si';
+   while(respuesta)
+   {
+	contador++
+	numero=prompt("Ingrese numero correcto: ");
+	numero=parseInt(numero);
+	acumulador=acumulador+contador;
 
+	respuesta=confirm();
 
-document.getElementById('suma').value=positivo;
-document.getElementById('producto').value=negativo;
-
-}//FIN DE LA FUNCIÓN
+	if(numero>=0)
+	{
+	acumuladorSuma=acumulador+numero;
+	} 
+	else
+	{
+  acumuladorMultipliacion=acumulador*numero;
+	}
+    
+   }
+   
+document.getElementById('suma').value=acumuladorSuma;
+document.getElementById('producto').value=acumuladorMultiplicacion;
+}
