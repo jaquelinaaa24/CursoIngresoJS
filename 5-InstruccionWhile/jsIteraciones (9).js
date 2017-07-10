c19c1788afed9bd6bc47c62eb127a2fed21f1f0d
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-10 12:22:47 -0300

    Update jsIteraciones (9).js

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,46 @@
+/*Al presionar el botón pedir números 
+hasta que el usuario quiera, mostrar el número máximo y el número mínimo.*/
+
+
 function Mostrar()
 {
-
-	var contador=0;
-	// declarar variables
+ var respuesta="si";
+ var max;
+ var min;
+ var numero;
+ var primera=true;
 	
-	var respuesta='si';
-
-	while(respuesta!='no')
+	while(respuesta)
 	{
-		
-	
+	numero=prompt("Ingrese numeros correctos");
+	numero=parseInt(numero);
+	 respuesta=confirm();
+
+    if(primera)
+	{
+    max=numero;
+	min=numero;
+	primera=false;
+
 	}
+	 else
+	{
+	 if(numero<min)
+	 {
+	 min=numero;
+     }
+	 if(numero>max)
+	 {
+     max=numero;
+	 }
 
 
-
-
-}//FIN DE LA FUNCIÓN
+}
+document.getElementById('maximo').value=max;
+document.getElementById('minimo').value=min;
+	}
+}
+/*LA BANDERA ES UNA VARIABLE QUE SIRVE PARA SAER CUANDO ES LA PRIMERA VEZ DE ALGO. esta formula se utiliza cuando no tenemos los datos de los maximos y minimos si tenemos
+ejemplo dos notas si. el maximo y minimo cuado empieza este problema no tienen valores
+entonces como no podemos hacer eso tenemos que hacer que la priemra vez que entren esos valores tomen valoresy lo hacemos con una bander aque la creamos en true si primera es true 
+tnonces es el primer numero que entra va hacer maximo o minimo y se lo asigno a maximo y minimo*/
