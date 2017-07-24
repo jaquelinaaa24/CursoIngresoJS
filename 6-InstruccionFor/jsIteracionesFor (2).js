c8ce1d5d4f53b8221487e32c7bc6f71aa8eed6d7
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-24 12:21:14 -0300

    Update jsIteracionesFor (2).js

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,14 @@
+/*al presionar el botón mostrar 10 
+repeticiones con números DESCENDENTES, desde el 10 al 1.*/
+
+
 function Mostrar()
 {
+var numeros;
 
+for(numeros=10 ; numeros>=1 ; numeros-- )
+{
+ alert(numeros);
+}
 
 }
