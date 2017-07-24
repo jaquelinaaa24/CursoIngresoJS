e9fb564da1b2f09a6219024ba6823d2c0884c246
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-24 12:22:29 -0300

    Update jsIteracionesFor (6).js

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,21 @@
+/*al presionar el botón pedir un número. 
+Mostar los numeros pares desde 
+el 1 al número ingresado, y mostar la 
+cantidad de numeros pares encontrados.*/
+
+
 function Mostrar()
 {
+numero=prompt("Por favor ingrese un numero");
+var contador;
 
+for(i=1 ; i<=numero ; i++) //estamos recorriendo desde el 1 hasta el numero que ingresa el usuario
+{
+ if(i%2==0)
+ {
+ 
+ }
 
-
-
-}//FIN DE LA FUNCIÓN
+     
+ }
+ }
