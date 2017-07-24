e8f874b1c6e129fbe8fc520e77e2319ab29b9831
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-24 12:23:15 -0300

    Update jsIteracionesFor (8).js

--- 6-InstruccionFor/jsIteracionesFor (8).js
+++ 6-InstruccionFor/jsIteracionesFor (8).js
@@ -1,7 +1,51 @@
+/*al presionar el botón pedir un número.
+ Informar si el numero es PRIMO o no.*/
+//es primo cuando tiene solo dos divisores por si mismo y por uno , si tiene un tercer divisor no es primo
+// */
 function Mostrar()
 {
+var numero=prompt("Ingrese un numero");
+var primo=true;
+
+for(i=2 ; i<numero ; i++)
+{
+if(numero%i==0)
+{
+primo=false
+break; //cuando entra una vez al if ya sabemos que no es primo; //utilizamos la bandera primo
+}
+
+}
+if(!primo)
+{
+alert("Este numero no es primo");
+}
+else
+{
+alert("Es primo");
+}
+}
 
 
 
+/*otra forma de hacerlo:
 
-}//FIN DE LA FUNCIÓN
+function Mostrar()
+{
+var numero=prompt();
+var contador=0;
+
+for(i=1 ; i<numero ; i++)
+{
+if(numero%i==0)
+{
+contador++;
+}
+}
+if(contador>2)
+{
+alert();
+}
+else
+{
+alert("Es primo");*/
