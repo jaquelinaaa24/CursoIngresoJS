89b1469872e34f625dd88e3482b3827f7a03a332
jaquelinaaa24 <jaquelinalopezabramo30@gmail.com>
2017-07-24 12:22:07 -0300

    Update jsIteracionesFor (5).js

--- 6-InstruccionFor/jsIteracionesFor (5).js
+++ 6-InstruccionFor/jsIteracionesFor (5).js
@@ -1,7 +1,14 @@
+/*al presionar el botón repetir el pedido 
+de número hasta que ingresemos el 9.*/
+
 function Mostrar()
 {
-
-
-
-
-}//FIN DE LA FUNCIÓN
+var clave;
+clave=prompt("Ingrese la clase");
+for(; clave!=9 ;)
+{
+ 
+ clave=prompt("Por favor ingrese el numero correcto");   
+}
+alert("Felicidades a ingresado el numero correcto");
+}
